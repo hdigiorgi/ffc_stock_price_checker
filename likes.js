2eb8fa33ec717ec46a4a8d28626b8555d265cb4d
hdigiorgi <me@hdigiorgi.com>
2019-02-18 12:56:26 -0300

    implement testing

--- likes.js
+++ likes.js
@@ -16,8 +16,6 @@ const Like = model('stock_likes', LikeSchema);
 function isLikedBy(_id, who) {
     return Like.find({_id, likedBy: {$all: who}})
         .exec().then(found => {
-            console.log(`is liked ${_id} ${who}`)
-            console.log(found)
             return found != null && found.length > 0;
         })
 }
@@ -39,11 +37,13 @@ function addLike(_id, who) {
 
 function getLikeCount(_id) {
     return Like.findById(_id).exec().then(x => {
-        console.log(_id)
-        console.log(x)
         if(x != null) return x.likeCount;
         else return 0;
     })
 }
 
-module.exports = {isLikedBy, addLike, getLikeCount};
+function deleteLikes(_id) {
+    return Like.deleteOne({_id}).exec()
+}
+
+module.exports = {isLikedBy, addLike, getLikeCount, deleteLikes};
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -10,39 +10,89 @@ var chaiHttp = require('chai-http');
 var chai = require('chai');
 var assert = chai.assert;
 var server = require('../server');
+var likes = require('../likes');
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
     
     suite('GET /api/stock-prices => stockData object', function() {
-      
+      this.timeout(8000);
+
       test('1 stock', function(done) {
        chai.request(server)
         .get('/api/stock-prices')
         .query({stock: 'goog'})
         .end(function(err, res){
-          
-          //complete this one too
-          
+          assert.exists(res.body)
+          assert.exists(res.body.stockData)
+          const stockData = res.body.stockData;
+          assert.isString(stockData.stock)
+          assert.strictEqual(stockData.stock, 'goog')
+          assert.isAbove(stockData.price, 0)
+          assert.isAtLeast(stockData.likes, 0)
           done();
         });
       });
       
       test('1 stock with like', function(done) {
-        
+        likes.deleteLikes('goog').then(_ => {
+          chai.request(server)
+          .get('/api/stock-prices')
+          .query({stock: 'goog', like: true})
+          .end(function(err, res){
+            const stockData = res.body.stockData;
+            assert.isAbove(stockData.price, 0)
+            assert.isAtLeast(stockData.likes, 1)
+            done();
+          });
+        })
       });
       
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({stock: 'goog', like: true})
+          .end(function(err, res){
+            const stockData = res.body.stockData;
+            assert.isAbove(stockData.price, 0)
+            assert.isAtLeast(stockData.likes, 1)
+            done();
+          });
       });
       
       test('2 stocks', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({stock: ['goog', 'amd']})
+          .end(function(err, res){
+            const stockData = res.body.stockData;
+            assert.isArray(stockData)
+            assert.isAbove(stockData[0].price, 0)
+            assert.isAbove(stockData[1].price, 1)
+            assert.strictEqual(stockData[0].stock, 'goog')
+            assert.strictEqual(stockData[1].stock, 'amd')
+            done();
+          });
       });
       
       test('2 stocks with like', function(done) {
-        
+        Promise.all([likes.deleteLikes('goog'), likes.deleteLikes('amd')]).then(_ => {
+          chai.request(server)
+            .get('/api/stock-prices')
+            .query({stock: ['goog', 'amd']})
+            .end(function(err, res){
+              const stockData = res.body.stockData;
+              assert.isArray(stockData)
+              assert.isAbove(stockData[0].price, 0)
+              assert.isAbove(stockData[1].price, 1)
+              assert.strictEqual(stockData[0].stock, 'goog')
+              assert.strictEqual(stockData[1].stock, 'amd')
+              assert.equal(stockData[0].rel_likes, 0)
+              assert.equal(stockData[1].rel_likes, 0)
+              done();
+            });
+        })
       });
       
     });
